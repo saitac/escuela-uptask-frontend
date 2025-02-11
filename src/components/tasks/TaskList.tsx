78e12ae3cd351c0379a95579a80c7acc917171e8
salvador <salvadoraita@gmail.com>
2025-02-11 17:55:46 -0300

    20250211 18:00

--- src/components/tasks/TaskList.tsx
+++ src/components/tasks/TaskList.tsx
@@ -1,4 +1,5 @@
 import { Task, taskStatus } from "@/classes/index"
+import { object } from "zod";
 
 type TaskListProps = {
     tasks: Task[]
@@ -6,19 +7,48 @@ type TaskListProps = {
 
 const TaskList = ({tasks}: TaskListProps) => {
     
-    const groupedTasks = tasks.reduce((acc, task) => {
-        /*let currentGroup = acc[task.status] ? [...acc[task.status]] : [];
-        currentGroup = [...currentGroup, task]
-        return { ...acc, [task.status]: currentGroup };*/
+
+    
+
+    const groupedTasks1 = () => {
+        //let x: [taskStatus, Task[]] = [tasks[0].status, []];
+        //let x: [taskStatus, Task[]];
+        let grouped: [taskStatus, Task[]][]
+
         
-        //let currentGroup = task.status in acc ? [...acc[task.status]] : [];
+        
+
+    }
+
+    /*const groupedTasks = tasks.reduce((acc: object, task) => {
+        let currentGroup = acc[task.status] ? [...acc[task.status]] : [];
+        currentGroup = [...currentGroup, task]
+        return { ...acc, [task.status]: currentGroup };
+        
+        //let currentGroup = task.status in acc ? acc["dd"] : [];
         //currentGroup = [...currentGroup, task]
         
-        //console.log(currentGroup);
-        return []
-    }, []);
+        
+        //let x = task.status in acc ? [...acc[task.status]] : acc[0]
+        //const x = acc?[task.status]: ""
 
-    console.log(groupedTasks)
+        
+        
+        console.log({...acc, [task.status]: []});
+        
+        
+    
+
+        //console.log(currentGroup);
+        
+        
+        
+        return {...acc, [task.status]: []}
+    }, []);*/
+
+    console.log(groupedTasks1)
+
+
     return (
         <>
             HOLA
