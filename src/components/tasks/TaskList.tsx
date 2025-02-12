3d123f41226e0e298dbdc51fa8842cb7cf94c3ee
salvador <salvadoraita@gmail.com>
2025-02-12 17:48:44 -0300

    20250212 17:48

--- src/components/tasks/TaskList.tsx
+++ src/components/tasks/TaskList.tsx
@@ -1,57 +1,47 @@
 import { Task, taskStatus } from "@/classes/index"
-import { object } from "zod";
+import TaskCard from "./TaskCard"
 
 type TaskListProps = {
     tasks: Task[]
 }
 
+type GroupedTasks = {
+    [key: string]: Task[]
+}
+
+const initialStatusGroups: GroupedTasks = {
+    [taskStatus.COMPLETED]: [],
+    [taskStatus.IN_PROGRESS]: [],
+    [taskStatus.ON_HOLD]: [],
+    [taskStatus.PENDING]: [],
+    [taskStatus.UNDER_REVIEW]: []
+}
+
 const TaskList = ({tasks}: TaskListProps) => {
-    
 
-    
-
-    const groupedTasks1 = () => {
-        //let x: [taskStatus, Task[]] = [tasks[0].status, []];
-        //let x: [taskStatus, Task[]];
-        let grouped: [taskStatus, Task[]][]
-
-        
-        
-
-    }
-
-    /*const groupedTasks = tasks.reduce((acc: object, task) => {
+    const groupedTasks = tasks.reduce((acc, task) => {
         let currentGroup = acc[task.status] ? [...acc[task.status]] : [];
         currentGroup = [...currentGroup, task]
         return { ...acc, [task.status]: currentGroup };
-        
-        //let currentGroup = task.status in acc ? acc["dd"] : [];
-        //currentGroup = [...currentGroup, task]
-        
-        
-        //let x = task.status in acc ? [...acc[task.status]] : acc[0]
-        //const x = acc?[task.status]: ""
-
-        
-        
-        console.log({...acc, [task.status]: []});
-        
-        
     
-
-        //console.log(currentGroup);
-        
-        
-        
-        return {...acc, [task.status]: []}
-    }, []);*/
-
-    console.log(groupedTasks1)
-
-
+    }, initialStatusGroups);
+    
     return (
         <>
-            HOLA
+            <h2 className="text-5xl font-black my-10">Tareas</h2>
+            <div className='flex gap-5 overflow-x-scroll 2xl:overflow-auto pb-32'>
+                {Object.entries(groupedTasks).map(([status, tasks]) => (
+                    <div key={status} className='min-w-[300px] 2xl:min-w-0 2xl:w-1/5'>
+                        <ul className='mt-5 space-y-5'>
+                            {tasks.length === 0 ? (
+                                <li className="text-gray-500 text-center pt-3">No Hay tareas</li>
+                            ) : (
+                                tasks.map(task => <TaskCard key={task._id} task={task} />)
+                            )}
+                        </ul>
+                    </div>
+                ))}
+            </div>
         </>
     )
 }
